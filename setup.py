7be7bc702ce1edaea8ccd4b4ed0646aef78e5721
Kirill Mitkovskii <kirill.mitkovskii@gmail.com>
2023-04-29 23:01:33 -0400

    add empty package

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+package_name = 'carla_tesla_model3_description'
+
+setup(
+    name=package_name,
+    version='0.1.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='robo',
+    maintainer_email='kirill.mitkovskii@gmail.com',
+    description='tesla.model3 CARLA vehicle model description',
+    license='MIT',
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
